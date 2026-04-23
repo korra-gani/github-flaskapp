abbc6f3614958b446c9a00011f49f8e8b66116df
korra-gani <korraganesh9490@gmail.com>
2026-04-23 13:56:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Flask App Running"
+    return "Hello Speshway Solutions"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
