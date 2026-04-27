38fb098a007c97bf306d63ca24900572e10adeb3
korra-gani <korraganesh9490@gmail.com>
2026-04-27 16:24:02 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+echo "Updating system..."
 yum update -y
+
+echo "Installing Docker..."
 yum install docker -y
+
+echo "Starting Docker..."
 service docker start
+
+echo "Enabling Docker..."
+chkconfig docker on
+
+echo "Adding ec2-user to docker group..."
+usermod -aG docker ec2-user
+
+echo "Install completed!"
